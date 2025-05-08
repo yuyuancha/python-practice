510766bd99c15f57982c970de42eae2a38a4c429
sizuku <sizuku.tsai@baifu-tech.net>
2025-05-08 15:11:42 +0800

    修改 hello.py 內容

--- hello.py
+++ hello.py
@@ -1,10 +1,1 @@
-import numpy as np
-# 宣告 ndarray 變數
-arr1 = np.array([1, 2, 3])                    
-# 輸出 ndarray
-print(arr1)                                   
-# 輸出 True
-print(type(arr1) == np.ndarray)               
-# 輸出 int64
-print(arr1.dtype)                             
-print()
+print('hello world')
