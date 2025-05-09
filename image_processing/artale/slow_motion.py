5ba36705911861ca1aa38b896f5d4d6967a41255
sizuku <sizuku.tsai@baifu-tech.net>
2025-05-09 16:59:52 +0800

    新增快速使用字串

--- image_processing/artale/slow_motion.py
+++ image_processing/artale/slow_motion.py
@@ -1,5 +1,6 @@
 from logic.video_tool import generate_slow_motion
 
-source_video_name = 'oiioi'
+#source_video_name = 'oiioi'
+source_video_name = 'siterle'
 slow_factor = 16  # 慢動作倍數: 1/16
 generate_slow_motion(source_video_name, slow_factor=slow_factor)
